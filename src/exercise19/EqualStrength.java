85af4c0bb27f272f187be8bb3bc8c7a076434e2a
Paige Ruppel <paige.ruppel@gmail.com>
2017-06-25 16:11:55 -0400

    got rid of maps, simplified to two simple if statements

--- src/exercise19/EqualStrength.java
+++ src/exercise19/EqualStrength.java
@@ -1,29 +1,21 @@
 package exercise19;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EqualStrength {
 
 	public boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
 		
-		int[] strengths = {yourLeft, yourRight, friendsLeft, friendsRight};
-		Map<Integer, String> strengthMap = new HashMap<>();
-		
-			for (int ind = 0 ; ind < strengths.length; ind++) {
-				if (!strengthMap.isEmpty() && strengthMap.containsKey(strengths[ind])) {
-					strengthMap.remove(strengths[ind]);
-				} else {
-					strengthMap.put(strengths[ind], " ");
-				}
-				
-				if (yourLeft == yourRight && friendsLeft == friendsRight && yourLeft + yourRight != friendsLeft + friendsRight) {
-					return false;
-				}
-				else if (strengthMap.isEmpty()) {
-					return true;
-				}
-			}
+		if (yourLeft==friendsLeft && yourRight == friendsRight) {
+			return true;
+		} else if (yourLeft==friendsRight && yourRight==friendsLeft) {
+			return true;
+		}
+	
 		return false;
 	}
 
