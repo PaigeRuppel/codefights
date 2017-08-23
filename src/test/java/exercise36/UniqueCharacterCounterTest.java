d84cd04754de167e108e3a7edf74d9ee00ee012e
paigeruppel <paige.ruppel@gmail.com>
2017-08-23 17:43:38 -0400

    wrote and passed 2 additional tests for unique char counter

--- src/test/java/exercise36/UniqueCharacterCounterTest.java
+++ src/test/java/exercise36/UniqueCharacterCounterTest.java
@@ -9,14 +9,24 @@ import org.junit.Test;
 public class UniqueCharacterCounterTest {
 
 	private UniqueCharacterCounter underTest;
-	
+
 	@Before
 	public void setup() {
 		underTest = new UniqueCharacterCounter();
 	}
-	
+
 	@Test
 	public void shouldReturn3FromAbc() {
 		assertThat(underTest.differentSymbolsNaive("abc"), is(3));
+	}
+
+	@Test
+	public void shouldReturn4FromAbcdabc() {
+		assertThat(underTest.differentSymbolsNaive("abcdabc"), is(4));
+	}
+
+	@Test
+	public void shouldReturn2FromAba() {
+		assertThat(underTest.differentSymbolsNaive("aba"), is(2));
 	}
 }
