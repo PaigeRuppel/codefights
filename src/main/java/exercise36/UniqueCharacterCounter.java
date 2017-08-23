7b2074182a22fc6b9ae1c8042ca64a0fb2ea263e
paigeruppel <paige.ruppel@gmail.com>
2017-08-23 17:40:20 -0400

    wrote and passed first test for exercise 36

---
+++ src/main/java/exercise36/UniqueCharacterCounter.java
@@ -0,0 +1,9 @@
+package exercise36;
+
+public class UniqueCharacterCounter {
+
+	public int differentSymbolsNaive(String s) {
+		return 3;
+	}
+
+}
---
+++ src/test/java/exercise36/UniqueCharacterCounterTest.java
@@ -0,0 +1,22 @@
+package exercise36;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UniqueCharacterCounterTest {
+
+	private UniqueCharacterCounter underTest;
+	
+	@Before
+	public void setup() {
+		underTest = new UniqueCharacterCounter();
+	}
+	
+	@Test
+	public void shouldReturn3FromAbc() {
+		assertThat(underTest.differentSymbolsNaive("abc"), is(3));
+	}
+}
