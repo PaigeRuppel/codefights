70adf428d543f00f9f0ae6c886a3d2fbfd7de714
paigeruppel <paige.ruppel@gmail.com>
2017-09-08 17:36:30 -0400

    started exercise 39

---
+++ src/main/java/exercise39/TreasureValueCalculator.java
@@ -0,0 +1,20 @@
+package exercise39;
+
+public class TreasureValueCalculator {
+
+	/**
+	 * You found two items in a treasure chest! The first item weighs weight1 and is
+	 * worth value1, and the second item weighs weight2 and is worth value2. What is
+	 * the total maximum value of the items you can take with you, assuming that
+	 * your max weight capacity is maxW and you can't come back for the items later?
+	 * 
+	 * Note that there are only two items and you can't bring more than one item of
+	 * each type, i.e. you can't take two first items or two second items.
+	 * 
+	 */
+	
+	public int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+
+	}
+	
+}
---
+++ src/test/java/exercise39/TreasureValueCalculatorTest.java
@@ -0,0 +1,13 @@
+package exercise39;
+
+import org.junit.Before;
+
+public class TreasureValueCalculatorTest {
+
+	private TreasureValueCalculator underTest;
+	
+	@Before
+	public void setup() {
+		underTest = new TreasureValueCalculator();
+	}
+}
