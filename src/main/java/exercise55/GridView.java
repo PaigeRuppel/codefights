a02a59fe42a45b8d0013a2d9ce3c2bdc98ef7cd5
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-20 09:04:11 -0400

    refactored to a hashset instead of map - passed solution

--- src/main/java/exercise55/GridView.java
+++ src/main/java/exercise55/GridView.java
@@ -1,7 +1,7 @@
 package exercise55;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GridView {
 
@@ -12,18 +12,16 @@ public class GridView {
 	
 	
 	public int differentSquares(int[][] matrix) {
-		//always a 2x2 square
-		Map<String, Integer> possibleSquares = new HashMap<>();
+		Set<String> possibleSquares = new HashSet<>();
 		int leftEdge = matrix[0].length - 1;
 		int bottomEdge = matrix.length - 1;
 		
 		for (int x = 0; x < leftEdge; x++) {
 			for (int y = 0; y < bottomEdge; y++) {
 					String square = "" + matrix[y][x] + matrix[y+1][x] + matrix[y][x+1] + matrix [y+1][x+1];
-					possibleSquares.put(square, 0);
+					possibleSquares.add(square);
 			}
 		}
-		
 		return possibleSquares.size();
 	}
 
