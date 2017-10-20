bb15d1a19ea94bbb4b646287553e68f80bbbd44c
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-20 08:40:55 -0400

    wrote second test and began boolean method for if a position is the corner of a possible square

--- src/main/java/exercise55/GridView.java
+++ src/main/java/exercise55/GridView.java
@@ -6,8 +6,27 @@ public class GridView {
 	 * Given a rectangular matrix containing only digits, calculate the number of
 	 * different 2 × 2 squares in it.
 	 */
+	
+	
 	public int differentSquares(int[][] matrix) {
+		//always a 2x2 square
+		
+		for (int x = 0; x < matrix[0].length; x++) {
+			for (int y = 0; y < matrix.length; y++) {
+//				if (//can move right one down one and diagonal one)
+			}
+		}
+		
+		//build square at each position and add to map 
+		
+		//entries on map will be distinct squares
+		
 		return 6;
+	}
+
+	public boolean isUpperLeftCornerOfSquare(int[][] matrix, int x, int y) {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 }
--- src/test/java/exercise55/GridViewTest.java
+++ src/test/java/exercise55/GridViewTest.java
@@ -1,7 +1,7 @@
 package exercise55;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,6 +19,20 @@ public class GridViewTest {
 	public void shouldReturn6DifferentSquaresTestCase1() {
 		int[][] matrix = { { 1, 2, 1 }, { 2, 2, 2 }, { 2, 2, 2 }, { 1, 2, 3 }, { 2, 2, 1 } };
 		assertThat(underTest.differentSquares(matrix), is(6));
+	}
+
+	@Test
+	public void shouldReturn1SquareTestCase2() {
+		int[][] matrix = { { 9, 9, 9, 9, 9 }, { 9, 9, 9, 9, 9 }, { 9, 9, 9, 9, 9 }, { 9, 9, 9, 9, 9 },
+				{ 9, 9, 9, 9, 9 } };
+		assertThat(underTest.differentSquares(matrix), is(1));
+	}
+	
+	@Test
+	public void shouldReturnTrueForPosition00() {
+		int[][] matrix = { { 9, 9, 9, 9, 9 }, { 9, 9, 9, 9, 9 }, { 9, 9, 9, 9, 9 }, { 9, 9, 9, 9, 9 },
+				{ 9, 9, 9, 9, 9 } };
+		assertTrue(underTest.isUpperLeftCornerOfSquare(matrix, 0, 0));
 	}
 
 }
