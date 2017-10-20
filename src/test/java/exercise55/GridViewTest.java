88b5bdfff6525e912066553ec1aaeea58009dcc1
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-20 09:02:06 -0400

    passed fifth test case and sixth extended test case

--- src/test/java/exercise55/GridViewTest.java
+++ src/test/java/exercise55/GridViewTest.java
@@ -34,7 +34,7 @@ public class GridViewTest {
 
 		assertThat(underTest.differentSquares(matrix), is(0));
 	}
-	
+
 	@Test
 	public void shouldReturn0SquaresTestCase4() {
 		int[][] matrix = { { 3, 4, 5, 6, 7, 8 } };
@@ -42,4 +42,19 @@ public class GridViewTest {
 		assertThat(underTest.differentSquares(matrix), is(0));
 	}
 
+	@Test
+	public void shouldReturn0SquaresTestCase5() {
+		int[][] matrix = { { 3 }, { 4 }, { 5 }, { 6 }, { 7 }, { 8 } };
+
+		assertThat(underTest.differentSquares(matrix), is(0));
+	}
+
+	@Test
+	public void shouldReturn54SquaresTestCase6() {
+		int[][] matrix = { { 2, 5, 3, 4, 3, 1, 3, 2 }, { 4, 5, 4, 1, 2, 4, 1, 3 }, { 1, 1, 2, 1, 4, 1, 1, 5 },
+				{ 1, 3, 4, 2, 3, 4, 2, 4 }, { 1, 5, 5, 2, 1, 3, 1, 1 }, { 1, 2, 3, 3, 5, 1, 2, 4 },
+				{ 3, 1, 4, 4, 4, 1, 5, 5 }, { 5, 1, 3, 3, 1, 5, 3, 5 }, { 5, 4, 4, 3, 5, 4, 4, 4 } };
+
+		assertThat(underTest.differentSquares(matrix), is(54));
+	}
 }
