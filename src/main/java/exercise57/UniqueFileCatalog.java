9276d156ae0b8f41aeb8abff68e4d0e273e70d6a
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-24 15:28:30 -0400

    began writing logic, not passing first two test cases

--- src/main/java/exercise57/UniqueFileCatalog.java
+++ src/main/java/exercise57/UniqueFileCatalog.java
@@ -1,11 +1,32 @@
 package exercise57;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UniqueFileCatalog {
 
+	/*
+	 * You are given an array of desired filenames in the order of their creation.
+	 * Since two files cannot have equal names, the one which comes later will have
+	 * an addition to its name in a form of (k), where k is the smallest positive
+	 * integer such that the obtained name is not used yet.
+	 * 
+	 * Return an array of names that will be given to the files.
+	 */
+	
 	public String[] fileNaming(String[] names) {
-		String[] output = {"doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"};
-		
-		return output;
+		List<String> uniqueNames = new ArrayList<>();
+		int count = 1;
+		for (int i = 0; i < names.length; i++ ) {
+			if (!uniqueNames.contains(names[i])) {
+				uniqueNames.add(i, names[i]);
+			} else {
+				count++;
+				uniqueNames.add(i, names[i] + "(" + count+ ")");
+			}
+		}
+		String[] answer = uniqueNames.toArray(new String[names.length]);
+		return answer;
 	}
 
 }
--- src/test/java/exercise57/UniqueFileCatalogTest.java
+++ src/test/java/exercise57/UniqueFileCatalogTest.java
@@ -9,16 +9,24 @@ import org.junit.Test;
 public class UniqueFileCatalogTest {
 
 	private UniqueFileCatalog underTest;
-	
+
 	@Before
 	public void setup() {
 		underTest = new UniqueFileCatalog();
 	}
-	
+
 	@Test
 	public void shouldReturnUniqueArrayTestCase1() {
-		String[] input = {"doc", "doc", "image", "doc(1)", "doc"};
-		String[] output = {"doc", "doc(1)", "image", "doc(1)(1)", "doc(2)"};
+		String[] input = { "doc", "doc", "image", "doc(1)", "doc" };
+		String[] output = { "doc", "doc(1)", "image", "doc(1)(1)", "doc(2)" };
+		assertThat(underTest.fileNaming(input), is(output));
+	}
+
+	@Test
+	public void shouldReturnUniqueArrayTestCase2() {
+		String[] input = { "a(1)", "a(6)", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a" };
+		String[] output = { "a(1)", "a(6)", "a", "a(2)", "a(3)", "a(4)", "a(5)", "a(7)", "a(8)", "a(9)", "a(10)",
+				"a(11)" };
 		assertThat(underTest.fileNaming(input), is(output));
 	}
 }
