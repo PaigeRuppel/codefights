bb4c73259fabeb44a499b2ca8fb3f1607843942d
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-24 08:32:41 -0400

    passing all tests - altered for loop logic to start with 9 and work down to 2

--- src/main/java/exercise56/FactoringTool.java
+++ src/main/java/exercise56/FactoringTool.java
@@ -10,27 +10,28 @@ public class FactoringTool {
 	 * integer the product of whose digits is equal to product. If there is no such
 	 * integer, return -1 instead.
 	 */
-	
+
 	public int digitsProduct(int product) {
 		List<Integer> factors = new ArrayList<>();
-		
+
 		int temp = product;
-		String converted = "-1";
-		
-		for(int i=2; i <= temp; i++){
-            while(temp % i==0){
-                factors.add(i);
-                temp = temp/i;
-                if (factors.get(0) * temp == product) {
-                	converted = factors.get(0) + "" + temp;
-                }
-            }
-        }
-		if (factors.contains(product)) {
+		String converted = "";
+
+		// try starting from 9 and working down to 2 instead... Will eliminate having duplicate 2s 3s etc 
+		for (int factor = 9; factor >= 2; factor--) {
+			while (temp % factor == 0) {
+				factors.add(0, factor);
+				temp = temp / factor;
+			}
+		}
+		if (factors.size() == 0) {
 			converted = "-1";
+		} else {
+			for (int i = 0; i < factors.size(); i++) {
+				converted += "" + factors.get(i);
+			}
 		}
 		return Integer.parseInt(converted);
-		
-	}
 
+	}
 }
