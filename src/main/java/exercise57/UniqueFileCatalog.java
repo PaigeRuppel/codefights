710a2d5cbfbfb59d56767fc5d9ea3daf512b0431
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-25 09:41:12 -0400

    not passing tests yet, getting stuck in a while loop that checks set entries

--- src/main/java/exercise57/UniqueFileCatalog.java
+++ src/main/java/exercise57/UniqueFileCatalog.java
@@ -1,6 +1,8 @@
 package exercise57;
 
+import java.util.Dictionary;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Set;
 
 public class UniqueFileCatalog {
@@ -13,14 +15,50 @@ public class UniqueFileCatalog {
 	 * 
 	 * Return an array of names that will be given to the files.
 	 */
-	
+
 	public String[] fileNaming(String[] names) {
-		Set<String> uniqueNames = new HashSet<>();
-		for (int i = 0; i < names.length; i++ ) {
-			uniqueNames.add(names[i]);
+		Set<String> uniqueNames = new HashSet<String>();
+
+		int k = 1;
+		String[] validNames = new String[names.length];
+		for (int i = 0; i < names.length; i++) {
+			if (!uniqueNames.contains(names[i])) {
+				uniqueNames.add(names[i]);
+				validNames[i] = names[i];
+			} 
 		}
-		String[] answer = uniqueNames.toArray(new String[names.length]);
-		return answer;
+		
+		for (int i = 0; i <names.length; i++) {
+			if (validNames[i] == null) {
+				k = 1;
+				String append = "(" + k + ")";
+			
+				while (uniqueNames.contains(names[i] + append)) {
+					k++;
+				}
+				
+				validNames[i] = names[i] + append;
+			}
+		}
+		
+//		for (int i = 0; i < names.length; i++) {
+//			if (!uniqueNames.contains(names[i])) {
+//				uniqueNames.put(names[i]);
+//				validNames[i] = names[i];
+//			} else {
+//				int k = 1;
+//				String append = "(" + k + ")";
+//				String checkKey = names[i] + append;
+//				while (uniqueNames.get(checkKey) != null) {
+//					k++;
+//				}
+//				uniqueNames.put(names[i] + append, names[i] + append);
+//				validNames[i] = names[i] + append;
+//			}
+//		}
+
+		// need a way to keep track of additions to names...
+		return validNames;
 	}
 
 }
