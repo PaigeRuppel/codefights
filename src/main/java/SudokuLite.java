bd887f68f39f3a588ea57b3c989d2be5c7368db0
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-31 13:52:54 -0400

    passed second test

--- src/main/java/SudokuLite.java
+++ src/main/java/SudokuLite.java
@@ -1,9 +1,47 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class SudokuLite {
 
-	public boolean sudoku(int[][] validGrid1) {
+	public boolean sudoku(int[][] grid) {
 		// TODO Auto-generated method stub
-		return true;
+		return isValidHorizontal(grid);
+	}
+
+	private boolean isValidHorizontal(int[][] grid) {
+		Set<Integer> values = buildPossibleNumbersSet();
+		boolean isValid = true;
+		for (int row = 0; row < grid.length; row++) {
+			for (int col = 0; col < grid[row].length; col++) {
+				int currentNumber = grid[row][col];
+				if (values.contains(currentNumber)) {
+					values.remove(currentNumber);
+				} else {
+					isValid = false;
+					break;
+				}
+			}
+			values = buildPossibleNumbersSet();
+		}
+		return isValid;
+	}
+	
+	private boolean isValidSmallerGrids(int[][] grid) {
+		 
+	}
+
+	private Set<Integer> buildPossibleNumbersSet() {
+		Set<Integer> oneThroughNine = new HashSet<>();
+		oneThroughNine.add(1);
+		oneThroughNine.add(2);
+		oneThroughNine.add(3);
+		oneThroughNine.add(4);
+		oneThroughNine.add(5);
+		oneThroughNine.add(6);
+		oneThroughNine.add(7);
+		oneThroughNine.add(8);
+		oneThroughNine.add(9);
+		return oneThroughNine;
 	}
 
 }
--- src/test/java/SudokuLiteTest.java
+++ src/test/java/SudokuLiteTest.java
@@ -16,6 +16,26 @@ public class SudokuLiteTest {
 	                         {2,4,3,6,5,7,1,9,8}, 
 	                         {8,1,9,3,2,4,7,6,5}};
 	
+	private int[][] invalidHorizontal = {{1,3,2,5,4,6,9,2,7}, 
+	                                {4,6,5,8,7,9,3,8,1}, 
+	                                {7,9,8,2,1,3,6,5,4}, 
+	                                {9,2,1,4,3,5,8,7,6}, 
+	                                {3,5,4,7,6,8,2,1,9}, 
+	                                {6,8,7,1,9,2,5,4,3}, 
+	                                {5,7,6,9,8,1,4,3,2}, 
+	                                {2,4,3,6,5,7,1,9,8}, 
+	                                {8,1,9,3,2,4,7,6,5}};
+	
+	private int[][] invalidSmallerGrids = {{1,3,4,2,5,6,9,8,7}, 
+	                                {4,6,8,5,7,9,3,2,1}, 
+	                                {7,9,2,8,1,3,6,5,4}, 
+	                                {9,2,3,1,4,5,8,7,6}, 
+	                                {3,5,7,4,6,8,2,1,9}, 
+	                                {6,8,1,7,9,2,5,4,3}, 
+	                                {5,7,6,9,8,1,4,3,2}, 
+	                                {2,4,5,6,3,7,1,9,8}, 
+	                                {8,1,9,3,2,4,7,6,5}};
+	
 	@Before
 	public void setup() {
 		underTest = new SudokuLite();
@@ -24,5 +44,15 @@ public class SudokuLiteTest {
 	@Test
 	public void shouldReturnTrue() {
 		assertTrue(underTest.sudoku(validGrid1));
+	}
+	
+	@Test
+	public void shouldReturnFalseTestCase2() {
+		assertFalse(underTest.sudoku(invalidHorizontal));
+	}
+	
+	@Test
+	public void shouldReturnFalseTestCase3() {
+		assertFalse(underTest.sudoku(invalidSmallerGrids));
 	}
 }
