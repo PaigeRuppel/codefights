e4b77de7dd960a1bfeb317b1202dee55b9366ee0
Paige Ruppel <paige.ruppel@gmail.com>
2017-10-31 13:38:36 -0400

    setup exercise 60 and passed first test

---
+++ src/main/java/SudokuLite.java
@@ -0,0 +1,9 @@
+
+public class SudokuLite {
+
+	public boolean sudoku(int[][] validGrid1) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
--- src/main/java/exercise59/SpiralMatrix.java
+++ src/main/java/exercise59/SpiralMatrix.java
@@ -2,8 +2,8 @@ package exercise59;
 
 public class SpiralMatrix {
 
-	int[][] spiralNumbers(int n) {
-		int[][] array = new int[n][n];
+	public int[][] spiralNumbers(int n) {
+		int[][] spiral = new int[n][n];
 
 		int left = 0;
 		int right = n - 1;
@@ -13,14 +13,14 @@ public class SpiralMatrix {
 		for (int counter = 1; counter <= n * n;) {
 			//start at the top and build out first row
 			for (int col = left; col <= right; col++) {
-				array[top][col] = counter++;
+				spiral[top][col] = counter++;
 			}
 			//increment top (so that what is already filled in will not be overwritten)
 			top++;
 
 			//start at remaining top for row and work down col
 			for (int row = top; row <= bottom; row++) {
-				array[row][right] = counter++;
+				spiral[row][right] = counter++;
 			}
 			
 			//decrement right so that filled in col will not be overwritten/edges move toward center
@@ -28,7 +28,7 @@ public class SpiralMatrix {
 
 			//start at the right edge and move towards the left
 			for (int col = right; col >= left; col--) {
-				array[bottom][col] = counter++;
+				spiral[bottom][col] = counter++;
 			}
 			
 			//decrement bottom edge/move towards center
@@ -36,13 +36,13 @@ public class SpiralMatrix {
 
 			//start at the bottom row and move up to the top edge to build left col
 			for (int row = bottom; row >= top; row--) {
-				array[row][left] = counter++;
+				spiral[row][left] = counter++;
 			}
 			
 			//increment left edge/move towards center
 			left++;
 		}
 
-		return array;
+		return spiral;
 	}
 }
